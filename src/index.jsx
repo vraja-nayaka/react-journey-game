0bc81c47e2e4ef7c6e5ad64b28ca927636b996fe
Vadim Tokar <tokar.web@biganto.com>
2022-04-26 21:31:47 +0300

    init

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import { render } from "react-dom";
+import { StrictMode, Suspense } from "react";
+import { Loader } from "@react-three/drei";
+import "./styles.css";
+import App from "./App";
+
+render(
+  <StrictMode>
+    <Suspense fallback={null}>
+      <App />
+    </Suspense>
+    <Loader />
+  </StrictMode>,
+  document.getElementById("root")
+);
